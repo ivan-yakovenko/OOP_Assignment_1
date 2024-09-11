e8c069005804ef5445c3b218e70fc628e94ddd13
Ivan Yakovenko <ivan2005y9@gmail.com>
2024-09-11 15:19:07 +0300

    added implementation for methods in ConfigReader class

--- Assignment_1/ConfigReader/ConfigReader.cpp
+++ Assignment_1/ConfigReader/ConfigReader.cpp
@@ -1,5 +1,40 @@
-//
-// Created by Ivan Yakovenko on 08.09.2024.
-//
-
 #include "ConfigReader.h"
+
+std::vector<Airplane> ConfigReader::getAirplanesInfo(std::string& filename) {
+    std::ifstream fin(filename);
+    std::string line;
+    std::vector<Airplane> airplanes;
+
+
+    while(std::getline(fin, line)) {
+        std::istringstream tokens(line);
+        std::string date, flightNo, price;
+        int seatsPerRow;
+        int startRow, endRow;
+        tokens >> date >> flightNo >> seatsPerRow;
+
+        if(seatsPerRow > 26) {
+            std::cerr << "Too many seats per row" << std::endl;
+            line.clear();
+            line.shrink_to_fit();
+            continue;
+        }
+        else {
+            std::unordered_map<std::string, std::string> priceForSeat;
+            while(tokens >> startRow >> endRow >> price) {
+                seatsGenerator(seatsPerRow, startRow, abs(endRow), price, priceForSeat);
+            }
+            Airplane airplane(date, flightNo, priceForSeat, {}, {});
+            airplanes.push_back(airplane);
+        }
+    }
+    return airplanes;
+}
+
+void ConfigReader::seatsGenerator(int seatsPerRow, int startRow, int endRow, std::string &price, std::unordered_map<std::string, std::string>& priceForSeat) {
+    for (int i = startRow; i <= endRow; i++) {
+        for (char j = 'A'; j < 'A' + seatsPerRow; j++) {
+            priceForSeat[std::to_string(i) + j] = price;
+        }
+    }
+}
