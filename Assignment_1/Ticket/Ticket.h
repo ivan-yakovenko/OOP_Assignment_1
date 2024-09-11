fd5647d23d93184fdd306890fa42346618dd57d5
Ivan Yakovenko <ivan2005y9@gmail.com>
2024-09-11 15:20:09 +0300

    added declaration for ticket class

---
+++ Assignment_1/Ticket/Ticket.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <iostream>
+
+class Ticket {
+private:
+    std::string date;
+    std::string flightNo;
+    std::string seat;
+    std::string price;
+    std::string username;
+    long bookedID;
+public:
+    Ticket();
+
+    Ticket(std::string date, std::string flightNo, std::string seat, std::string price, std::string username, long bookedID);
+
+//    std::string getDate() const;
+//
+//    std::string getFlightNo() const;
+
+    std::string getSeat() const;
+
+//    std::string getPrice() const;
+//
+//    std::string getUsername() const;
+
+    long getBookedID() const;
+
+    void setDate(std::string &date);
+
+    void setFlightNo(std::string &flightNo);
+
+    void setSeat(std::string &seat);
+
+    void setPrice(std::string &price);
+
+    void setUsername(std::string &username);
+
+    void setBookedID(long bookedID);
+
+    long generateID();
+
+    void printTicket() const;
+
+    void printForFlight() const;
+};
