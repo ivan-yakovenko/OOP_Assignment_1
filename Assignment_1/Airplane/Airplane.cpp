2bfdb37ce00398231d742be0892e6dc3d2e0456a
Ivan Yakovenko <ivan2005y9@gmail.com>
2024-09-14 20:20:00 +0300

    changed implementation

--- Assignment_1/Airplane/Airplane.cpp
+++ Assignment_1/Airplane/Airplane.cpp
@@ -4,30 +4,33 @@ Airplane::Airplane() : date("Empty"), flightNo("Empty"), priceForSeat({}), booke
 
 Airplane::Airplane(std::string date, std::string flightNo,
                    std::unordered_map<std::string, std::string> priceForSeat,
-                   std::unordered_map<std::string, std::string> bookedSeat, std::vector<Ticket> bookedTickets) : date(date),
-                                                                              flightNo(flightNo),
-                                                                              priceForSeat(priceForSeat),
-                                                                              bookedSeat(bookedSeat),
-                                                                              bookedTickets(bookedTickets){}
+                   std::unordered_map<std::string, std::string> bookedSeat,
+                   std::unordered_map<long, Ticket> bookedTickets) : date(date),
+                                                                     flightNo(flightNo),
+                                                                     priceForSeat(priceForSeat),
+                                                                     bookedSeat(bookedSeat),
+                                                                     bookedTickets(bookedTickets) {}
 
-//std::string Airplane::getDate() const {
-//    return date;
-//}
+std::string Airplane::getDate() const {
+    return this->date;
+}
+
 //
-//std::string Airplane::getFlightNo() const {
-//    return flightNo;
-//}
+std::string Airplane::getFlightNo() const {
+    return this->flightNo;
+}
+
 //
 //int Airplane::getSeatsPerRow() const {
 //    return seatsPerRow;
 //}
 //
-//std::unordered_map<std::string, std::string> Airplane::getPriceForSeat() const {
-//    return priceForSeat;
-//}
+std::unordered_map<std::string, std::string> Airplane::getPriceForSeat() const {
+    return this->priceForSeat;
+}
 
 //bool Airplane::getIsAvailable() const {
-//    return isAvailable;
+//    return this->isAvailable;
 //}
 
 //void Airplane::setDate(std::string &date) {
@@ -51,92 +54,73 @@ Airplane::Airplane(std::string date, std::string flightNo,
 //}
 
 
-std::vector<Ticket> Airplane::getBookedTickets() const {
+std::unordered_map<long, Ticket> Airplane::getBookedTickets() const {
     return this->bookedTickets;
 }
 
-void Airplane::generalPrint() const {
-    std::cout << "Date: " << date << std::endl;
-    std::cout << "Flight number: " << flightNo << std::endl;
-    for (const auto &seat: priceForSeat) {
-        std::cout << "Seat: " << seat.first << " Price: " << seat.second << std::endl;
-    }
-    std::cout << priceForSeat.size() << std::endl;
-}
+//void Airplane::generalPrint() const {
+//    std::cout << "Date: " << date << std::endl;
+//    std::cout << "Flight number: " << flightNo << std::endl;
+//    for (const auto &seat: priceForSeat) {
+//        std::cout << "Seat: " << seat.first << " Price: " << seat.second << std::endl;
+//    }
+//    std::cout << priceForSeat.size() << std::endl;
+//}
 
 void Airplane::printPlaces() const {
     for (const auto &seat: priceForSeat) {
         std::cout << "Seat: " << seat.first << " Price: " << seat.second << std::endl;
     }
+    std::cout << "PLaces left: " << priceForSeat.size() << std::endl;
 }
 
 void Airplane::check(std::string &date, std::string &flightNo) const {
-//    if (this->date == date && this->flightNo == flightNo) {
-//        printPlaces();
-//    }
-    std::cout << priceForSeat.size() << std::endl;
+    if (this->date == date && this->flightNo == flightNo) {
+        printPlaces();
+    }
 }
 
 void Airplane::book(std::string &date, std::string &flightNo, std::string &seat, std::string &username) {
-    if (this->date == date && this->flightNo == flightNo) {
-        if (priceForSeat.contains(seat)) {
-            Ticket ticket;
-            ticket.setDate(date);
-            ticket.setFlightNo(flightNo);
-            ticket.setSeat(seat);
-            ticket.setPrice(priceForSeat.at(seat));
-            ticket.setUsername(username);
-            ticket.setBookedID(ticket.generateID());
-            bookedSeat.insert(priceForSeat.extract(seat));
-            bookedTickets.emplace_back(ticket);
-        }
-    }
+    Ticket ticket;
+    long bookedID = ticket.generateID();
+    ticket.setDate(date);
+    ticket.setFlightNo(flightNo);
+    ticket.setSeat(seat);
+    ticket.setPrice(priceForSeat.at(seat));
+    ticket.setUsername(username);
+    ticket.setBookedID(bookedID);
+    std::cout << "Confirmed with ID: " << bookedID << std::endl;
+    bookedSeat.insert(priceForSeat.extract(seat));
+    bookedTickets[bookedID] = ticket;
 }
 
 void Airplane::returnTicket(long bookedID) {
-    for(auto iterator = bookedTickets.begin(); iterator != bookedTickets.end(); iterator++) {
-        if(iterator->getBookedID() == bookedID) {
-            priceForSeat.insert(bookedSeat.extract(iterator->getSeat()));
-            bookedTickets.erase(iterator);
-            break;
-        }
-        else {
-            std::cerr << "No such ID" << std::endl;
-        }
-    }
+    Ticket &ticket = bookedTickets.at(bookedID);
+    std::cout << "Confirmed " << ticket.getPrice() << " refund for " << ticket.getUsername() << std::endl;
+    priceForSeat.insert(bookedSeat.extract(ticket.getSeat()));
+    bookedTickets.erase(bookedID);
 }
+
 
 void Airplane::viewByID(long bookedID) {
-    for(auto iterator = bookedTickets.begin(); iterator != bookedTickets.end(); iterator++) {
-        if(iterator->getBookedID() == bookedID) {
-            iterator->printTicket();
-            break;
-        }
-        else {
-            std::cerr << "No such ID" << std::endl;
-        }
-    }
+    Ticket &ticket = bookedTickets.at(bookedID);
+    ticket.printTicket();
+
 }
 
-void Airplane::viewByUsername(std::string &username) {
-    for(auto iterator = bookedTickets.begin(); iterator != bookedTickets.end(); iterator++) {
-        if(iterator->getUsername() == username) {
-            iterator->printTicket();
-            break;
-        }
-        else {
-            std::cerr << "No such username" << std::endl;
-        }
-    }
-}
 
-void Airplane::viewByFlightNumber(std::string &flightNo) {
-    for(auto iterator = bookedTickets.begin(); iterator != bookedTickets.end(); iterator++) {
-        if(iterator->getFlightNo() == flightNo) {
-            iterator->printForFlight();
-        }
-        else {
-            std::cerr << "No such username" << std::endl;
-        }
-    }
-}
+//void Airplane::viewByUsername(std::string &username) {
+//    for (const auto &ticket: bookedTickets) {
+//        if (ticket.second.getUsername() == username) {
+//            ticket.second.printForUsername();
+//        }
+//    }
+//}
+//
+//void Airplane::viewByFlightNumber(std::string &flightNo) {
+//    for (const auto &ticket: bookedTickets) {
+//        if (ticket.second.getFlightNo() == flightNo) {
+//            ticket.second.printForFlight();
+//        }
+//    }
+//}
