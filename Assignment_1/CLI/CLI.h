069f31aa2b4fe80de86bcc8ee60d4389cbf6d1c6
Ivan Yakovenko <ivan2005y9@gmail.com>
2024-09-14 20:36:52 +0300

    added declaration for CLI class

--- Assignment_1/CLI/CLI.h
+++ Assignment_1/CLI/CLI.h
@@ -1,14 +1,20 @@
-//
-// Created by Ivan Yakovenko on 09.09.2024.
-//
-
-#ifndef OOP_ASSIGNMENT_1_CLI_H
-#define OOP_ASSIGNMENT_1_CLI_H
-
+#pragma once
+#include "../ConfigReader/ConfigReader.h"
 
 class CLI {
-
+private:
+    ConfigReader configReader;
+    std::string filePath;
+    std::vector<Airplane> airplanes;
+    void checkPlaces(std::string &date, std::string &flightNo);
+    void bookSeat(std::string &date, std::string &flightNo, std::string &seat, std::string &username);
+    void returnTicket(long bookedID);
+    void viewID(long bookedID);
+    void viewUsername(std::string &username);
+    void viewFlightNo(std::string &flightNo);
+    bool validCommand(std::string &command);
+    bool checkIfId(std::string &parameter);
+public:
+    CLI(std::string &filePath);
+    void run();
 };
-
-
-#endif //OOP_ASSIGNMENT_1_CLI_H
