2cc30112a72cb36cb518b9f28f07ceecb2a9a60d
Ivan Yakovenko <ivan2005y9@gmail.com>
2024-09-23 17:28:21 +0300

    added declaration for filehandler class

--- Assignment_1/FileHandler/FileHandler.h
+++ Assignment_1/FileHandler/FileHandler.h
@@ -1,14 +1,29 @@
-//
-// Created by Ivan Yakovenko on 21.09.2024.
-//
+#pragma once
 
-#ifndef OOP_ASSIGNMENT_1_FILEHANDLER_H
-#define OOP_ASSIGNMENT_1_FILEHANDLER_H
-
+#include <iostream>
+#include <fcntl.h>
+#include <unistd.h>
 
 class FileHandler {
+public:
+    FileHandler();
 
+    FileHandler(std::string &filename, int flag);
+
+    FileHandler(const FileHandler &other) = delete;
+
+    FileHandler operator=(const FileHandler &other) const = delete;
+
+    FileHandler(FileHandler &&other) noexcept;
+
+    FileHandler &operator=(FileHandler &&other) noexcept;
+
+    explicit operator int() const;
+
+    int getFD() const;
+
+private:
+    int fd;
 };
 
 
-#endif //OOP_ASSIGNMENT_1_FILEHANDLER_H
