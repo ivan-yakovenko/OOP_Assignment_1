4ccf119dfcca98b335c7759d803877d705017d6c
Ivan Yakovenko <ivan2005y9@gmail.com>
2024-09-23 17:28:47 +0300

    added implementation of filehand;er class

--- Assignment_1/FileHandler/FileHandler.cpp
+++ Assignment_1/FileHandler/FileHandler.cpp
@@ -1,5 +1,34 @@
-//
-// Created by Ivan Yakovenko on 21.09.2024.
-//
-
 #include "FileHandler.h"
+
+FileHandler::FileHandler() : fd(-1) {}
+
+FileHandler::FileHandler(std::string &filename, int flag) {
+    fd = open(filename.c_str(), flag);
+    if (fd == -1) {
+        throw std::runtime_error("Error, can not open the file");
+    }
+}
+
+FileHandler::FileHandler(FileHandler &&other) noexcept: fd(other.fd) {
+    other.fd = -1;
+}
+
+FileHandler &FileHandler::operator=(FileHandler &&other) noexcept {
+    if (this == &other) {
+        return *this;
+    }
+    if (this->fd != -1) {
+        close(this->fd);
+    }
+    this->fd = other.fd;
+    other.fd = -1;
+    return *this;
+}
+
+FileHandler::operator int() const {
+    return this->fd;
+}
+
+int FileHandler::getFD() const {
+    return this->fd;
+}
