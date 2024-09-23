76fb63a72c876a85d4110b9854daf57475dc5066
Ivan Yakovenko <ivan2005y9@gmail.com>
2024-09-23 17:30:06 +0300

    updated implementation of airplane class

--- Assignment_1/Airplane/Airplane.cpp
+++ Assignment_1/Airplane/Airplane.cpp
@@ -15,57 +15,18 @@ std::string Airplane::getDate() const {
     return this->date;
 }
 
-//
+
 std::string Airplane::getFlightNo() const {
     return this->flightNo;
 }
 
-//
-//int Airplane::getSeatsPerRow() const {
-//    return seatsPerRow;
-//}
-//
 std::unordered_map<std::string, std::string> Airplane::getPriceForSeat() const {
     return this->priceForSeat;
 }
 
-//bool Airplane::getIsAvailable() const {
-//    return this->isAvailable;
-//}
-
-//void Airplane::setDate(std::string &date) {
-//    this->date = date;
-//}
-//
-//void Airplane::setFlightNo(std::string &flightNo) {
-//    this->flightNo = flightNo;
-//}
-//
-//void Airplane::setSeatsPerRow(int seatsPerRo) {
-//    this->seatsPerRow = seatsPerRow;
-//}
-//
-//void Airplane::setPriceForSeat(std::unordered_map<std::string, std::string> &priceForSeat) {
-//    this->priceForSeat = priceForSeat;
-//}
-
-//void Airplane::setIsAvailable(bool isAvailable) {
-//    this->isAvailable = isAvailable;
-//}
-
-
 std::unordered_map<long, Ticket> Airplane::getBookedTickets() const {
     return this->bookedTickets;
 }
-
-//void Airplane::generalPrint() const {
-//    std::cout << "Date: " << date << std::endl;
-//    std::cout << "Flight number: " << flightNo << std::endl;
-//    for (const auto &seat: priceForSeat) {
-//        std::cout << "Seat: " << seat.first << " Price: " << seat.second << std::endl;
-//    }
-//    std::cout << priceForSeat.size() << std::endl;
-//}
 
 void Airplane::printPlaces() const {
     for (const auto &seat: priceForSeat) {
@@ -105,22 +66,4 @@ void Airplane::returnTicket(long bookedID) {
 void Airplane::viewByID(long bookedID) {
     Ticket &ticket = bookedTickets.at(bookedID);
     ticket.printTicket();
-
 }
-
-
-//void Airplane::viewByUsername(std::string &username) {
-//    for (const auto &ticket: bookedTickets) {
-//        if (ticket.second.getUsername() == username) {
-//            ticket.second.printForUsername();
-//        }
-//    }
-//}
-//
-//void Airplane::viewByFlightNumber(std::string &flightNo) {
-//    for (const auto &ticket: bookedTickets) {
-//        if (ticket.second.getFlightNo() == flightNo) {
-//            ticket.second.printForFlight();
-//        }
-//    }
-//}
